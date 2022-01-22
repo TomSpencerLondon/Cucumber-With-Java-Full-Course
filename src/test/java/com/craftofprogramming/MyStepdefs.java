4c64fc873dc9d5890206a8f3b7b2429e475a7eec
Tom <thomas.spencer@codurance.com>
2022-01-22 00:57:41 +0000

    Add step defs and gradle folder

--- src/test/java/com/craftofprogramming/MyStepdefs.java
+++ src/test/java/com/craftofprogramming/MyStepdefs.java
@@ -1,14 +1,17 @@
 package com.craftofprogramming;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import java.util.Optional;
 
 public class MyStepdefs {
   LibraryService libraryService;
   private Book book;
+  private Optional<Topic> topic;
 
   @Given("^That the LibraryService is initialized$")
     public void thatTheLibraryServiceIsInitialized() {
@@ -28,4 +31,14 @@ public class MyStepdefs {
         assertNotNull(book);
       }
     }
+
+  @When("I query the LibraryService for the the most popular topic")
+  public void iQueryTheLibraryServiceForTheTheMostPopularTopic() {
+    topic = libraryService.mostPopularTopic();
+  }
+
+  @Then("I get {}")
+  public void iGet(Topic actualTopic) {
+    assertEquals(actualTopic, topic.get());
+  }
 }
--- src/test/resources/com/craftofprogramming/QueryLibraryService.feature
+++ src/test/resources/com/craftofprogramming/QueryLibraryService.feature
@@ -4,3 +4,8 @@ Feature: Query books from the LibraryService
     Given That the LibraryService is initialized
     When I query the LibraryService for the "Effective Java" book
     Then It is "true" that I find it
+
+  Scenario: Query the LibraryService for the most popular topic
+    Given That the LibraryService is initialized
+    When I query the LibraryService for the the most popular topic
+    Then I get "COMPUTING"
